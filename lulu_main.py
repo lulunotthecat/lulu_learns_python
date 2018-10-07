96d155c8e87a4986c2511a49a8512aa5eeed13be
Lulupenguin <34438548+Lulupenguin@users.noreply.github.com>
2018-10-07 13:50:37 +0200

    done

--- lulu_main.py
+++ lulu_main.py
@@ -1,9 +1,29 @@
 #!/usr/bin/env python
 
 def main():
-	#here we work
-	print("Hello lulu")
+    #here we work
+    print("Hello lulu")
+    name = input("What is your name?")
+    print("hello " + name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
-	main()
+    main()
+
